d83062a2cb78ae3faba35e67799b3eb2852d69c3
Nithinn Manickam <x-nithinn.manickam@modelon.com>
2023-06-26 10:17:57 +0100

    added getWorkSpacesID endpoint

--- src/api.ts
+++ src/api.ts
@@ -278,6 +278,20 @@ class Api {
                 .catch((e) => reject(toApiError(e)))
         })
     }
+    getWorkSpacesID = async (param:string): Promise<ExperimentDefinition[]> => {
+        return new Promise((resolve, reject) => {
+            this.ensureImpactToken()
+            .then(() => {
+                this.axios
+                    .get(
+                        `${this.baseUrl}${this.jhUserPath}impact/api/workspaces/${param}`
+                    )
+                    .then((response) => resolve(response.data?.definition))
+                    .catch((e) => reject(toApiError(e)))
+            })
+            .catch((e) => reject(toApiError(e)))
+    })
+    }
 
     createExperiment = async ({
         experimentDefinition,
